da1ccae2071e43baf7b27ba6357acb7aae4e10e5
BinkANDr <maximo.cosme.malasquez@gmail.com>
2019-12-29 00:00:20 -0500

    agregado de clase

---
+++ mavenproject2/src/main/java/HoliMundo.java
@@ -0,0 +1,36 @@
+/*
+
+I still think about you  :')
+ */
+public class HoliMundo {
+
+    public static void main(String[] args) {
+
+        var usuario = "Células";
+        System.out.println(usuario);
+        var tipop = "Procariota";
+        var tipoe = "Eucariota"
+        System.out.println("Se dividen en : " + tipop + " y " + tipoe
+        );
+        var p = " Animal y Vegetal"
+        var Eucariota = " Es de tipo célula" + p;
+        var Procariota = " Pertenece a Reino Monera";
+        System.out.println("1. " + tipop + Procariota);
+        System.out.println("2. " + tipoe + Eucariota);
+        String Car = "Características de ";
+        var Cel = " Membrana Celular."
+        var fount = " Sus fuentes de nutrición"
+        var carb = "Energía"
+        System.out.println(Car +  tipop + ":" + " Carece de " + Cel + " Asímismo," + fount + " son por : " + carb);
+        var primer = "Fotótrofo"
+        var fuente1 = ": Su fuente de energía es el Sol."
+        var n = " Realizado por bacterías"
+        var tipo1 = " Verdes , purpuras y Cianobacterías "
+        var nutriciónb = "Fuentes de carbono"
+        System.out.println("Dividido en :")
+        System.out.println(primer + fuente1 + "Esto es " + n + ":" + tipo1) 
+        var segundo = "Quimiótrofo"
+        System.out.println(segundo + fuente1 + n + " Sulfurosas,ferrobacterias y nitrobacterias");
+        System.out.println(Car + tipoe + ": tiene " + Cel +);
+     
+    }
